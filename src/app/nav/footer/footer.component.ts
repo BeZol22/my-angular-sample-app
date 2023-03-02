0092c223c15c6c3b41d717853105344cc3227dc7
Zoltan Beke <zoltan.beke89@gmail.com>
2023-03-02 22:43:11 +0100

    Fix footer to the bottom. Copyright with dynamic date.

--- src/app/nav/footer/footer.component.ts
+++ src/app/nav/footer/footer.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  public getCurrentYear(): string {
+    const startDate: number = 2023;
+    const date: number = new Date().getFullYear();
 
+    if (date !== startDate) {
+      return `${startDate}-${date}`;
+    }
+    return date.toString();
+  }
 }
