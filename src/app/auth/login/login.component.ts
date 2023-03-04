47c72fc10064bdac9ae1535469d3ade74bb3e258
Zoltan Beke <zoltan.beke89@gmail.com>
2023-03-05 00:49:30 +0100

    Finetune MatSnackBar

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,7 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -34,14 +34,20 @@ export class LoginComponent implements OnInit {
     }
 
     // Perform login action here
-    const email = this.loginForm.controls['email'].value;
-    const password = this.loginForm.controls['password'].value;
 
-    this.snackBar.open(`Logged in with email: ${email}`, '', {
-      duration: 2000,
-    });
+    // Notificationmessage for user
+    this.openMatSnackBar(`Logged in with email: ${this.email.value}`, '');
   }
 
+  // Config for Notificationmessage
+  openMatSnackBar(message: string, action: string) {
+    let config = new MatSnackBarConfig();
+    config.duration = 3000;
+    config.horizontalPosition = 'center';
+    this.snackBar.open(message, action, config);
+  }
+
+  // Get FormControls
   get email(): FormControl {
     return this.loginForm.get('email') as FormControl;
   }
