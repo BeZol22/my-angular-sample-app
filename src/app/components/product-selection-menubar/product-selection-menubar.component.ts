bb8ce25a01a2625b942a90ab5c9d47e3b315c8a9
Zoltan Beke <zoltan.beke89@gmail.com>
2023-05-29 14:32:11 +0200

    CSS for selection

--- src/app/components/product-selection-menubar/product-selection-menubar.component.ts
+++ src/app/components/product-selection-menubar/product-selection-menubar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-selection-menubar',
   templateUrl: './product-selection-menubar.component.html',
-  styleUrls: ['./product-selection-menubar.component.scss']
+  styleUrls: ['./product-selection-menubar.component.scss'],
 })
 export class ProductSelectionMenubarComponent {
+  public selectedMenu: string = '';
 
+  selectMenu(menu: string): void {
+    this.selectedMenu = menu;
+  }
 }
