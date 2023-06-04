35ec2ec8a64d2ab1b88129bb434720a7db829757
Zoltan Beke <zoltan.beke89@gmail.com>
2023-06-04 14:58:43 +0200

    Fix for "saving" selected search when navigating back

--- src/app/components/product-selection-menubar/product-selection-menubar.component.ts
+++ src/app/components/product-selection-menubar/product-selection-menubar.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/state/app.reducer';
 import { selectSearch } from 'src/app/state/search.actions';
-import { SearchState } from 'src/app/state/search.reducer';
 
 @Component({
   selector: 'app-product-selection-menubar',
   templateUrl: './product-selection-menubar.component.html',
   styleUrls: ['./product-selection-menubar.component.scss'],
 })
-export class ProductSelectionMenubarComponent {
+export class ProductSelectionMenubarComponent implements OnInit {
   @Output() searchSelected: EventEmitter<string> = new EventEmitter<string>();
   public selectedSearch: string = '';
 
-  constructor(private store: Store<SearchState>) {}
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store
+      .select((state) => state.search.selectedSearch)
+      .subscribe((selectedSearch) => {
+        this.selectedSearch = selectedSearch;
+      });
+  }
 
   // selectSearch(search: string): void {
   //   this.selectedSearch = search;
